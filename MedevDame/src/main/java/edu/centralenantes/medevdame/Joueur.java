75b62c8d581486011569fd57cda646bd104aefdc
Boulanger <alexandre-boulanger@bbox.fr>
2021-12-09 11:57:32 +0100

    Ajout de squelette de joueur

---
+++ MedevDame/src/main/java/edu/centralenantes/medevdame/Joueur.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.medevdame;
+
+/**
+ *
+ * @author Boulanger
+ */
+public class Joueur {
+    boolean couleur;
+    
+    public Joueur(){
+        
+    }
+    private void Jouer(){
+        
+    }
+    
+    
+}
