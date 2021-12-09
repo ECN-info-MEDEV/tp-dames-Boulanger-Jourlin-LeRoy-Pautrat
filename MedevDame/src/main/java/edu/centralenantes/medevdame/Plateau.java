587a241d6392c6bcd3f73e0eb4b643b6ec8c6762
Louis Pautrat <louis.pautrat@eleves.ec-nantes.fr>
2021-12-09 12:10:25 +0100

    ajout du squelette de la classe Plateau

---
+++ MedevDame/src/main/java/edu/centralenantes/medevdame/Plateau.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.medevdame;
+
+/**
+ *
+ * @author Louis
+ */
+public class Plateau {
+    
+    Piece[][] liste_de_cases;
+    Joueur joueur1;
+    Joueur joueur2;
+    
+    public void tourDeJeu(){
+        
+    }
+    
+    public void afficher(){
+        
+    }
+    
+}
