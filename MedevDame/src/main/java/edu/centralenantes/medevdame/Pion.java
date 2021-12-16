ff51df4afaf244209ba35d1716e8ec7ed07c5b08
Anne-So <anne-sophie.jourlin@eleves.ec-nantes.fr>
2021-12-16 11:39:21 +0100

    Javadoc

--- MedevDame/src/main/java/edu/centralenantes/medevdame/Pion.java
+++ MedevDame/src/main/java/edu/centralenantes/medevdame/Pion.java
@@ -46,5 +46,10 @@ public class Pion extends Piece {
         }
         return result;
     }
+
+    @Override
+    public boolean capturePossible() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
