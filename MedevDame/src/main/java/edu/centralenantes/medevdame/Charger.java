523fc9dab19d7eb4a2b35b1351efcf54024beaa1
Lénaëlle LR <Lenaelle.Le-Roy@eleves.ec-nantes.fr>
2021-12-16 10:49:08 +0100

    Ajout de la classe Enregistrer

--- MedevDame/src/main/java/edu/centralenantes/medevdame/Charger.java
+++ MedevDame/src/main/java/edu/centralenantes/medevdame/Charger.java
@@ -6,9 +6,11 @@
 package edu.centralenantes.medevdame;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
- *
+ * Classe qui s'occupe de charger un fichier de sauvegarde et de générer un plateau correspondant 
  * @author lénaëlle
  */
 public class Charger {
@@ -21,6 +23,62 @@ public class Charger {
      * Le BufferedReader pour lire le fichier de sauvegarde
      */
     private BufferedReader lecteur;
+
+    /**
+     *
+     * @param nomFichier
+     */
+    public Charger(String nomFichier) {
+        this.nomFichier = nomFichier;
+        
+        try{
+            lecteur = new BufferedReader(new FileReader(nomFichier));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Cette fonction renvoie une instance de la classe Plateau avec les éléments rentrés dans le fichier de sauvegarde
+     * @return le plateau généré
+     */
+    public Plateau chargePartie(){
+        Plateau plateau = new Plateau();
+        
+        ArrayList<String> liste_lignes = chargeListeLignes();
+        
+        return plateau;
+        
+    }
+    
+     /**
+     * Fonction qui retourne une liste de string correspondant à la liste des lignes d'un fichier donné
+     * @return La liste de lignes du fichier
+     */
+    private ArrayList<String> chargeListeLignes(){
+        ArrayList<String> liste_de_lignes = new ArrayList<>();
+        
+        try{
+        String ligne;
+        ligne = lecteur.readLine();
+
+        while(ligne != null){
+            liste_de_lignes.add(ligne);
+            ligne = lecteur.readLine();
+        }
+        
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return liste_de_lignes;
+    }
+    
     
     
 }
+
+
+
+
+
---
+++ MedevDame/src/main/java/edu/centralenantes/medevdame/Enregistrer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.medevdame;
+
+import java.io.BufferedWriter;
+
+/**
+ *
+ * @author lénaëlle
+ */
+public class Enregistrer {
+        /**
+     * Le nom du fichier de sauvegarde
+     */
+    private String nomFichier;
+    
+    /**
+     * Le BufferedWriter nécessaire pour l'écriture du fichier
+     */
+    private BufferedWriter writer;
+}
