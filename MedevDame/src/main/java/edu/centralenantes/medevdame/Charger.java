2ff8ea8400d1e657266cfbec835585060f723ab0
Lénaëlle LR <Lenaelle.Le-Roy@eleves.ec-nantes.fr>
2021-12-16 10:36:13 +0100

    Ajout de la classe charger

---
+++ MedevDame/src/main/java/edu/centralenantes/medevdame/Charger.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.medevdame;
+
+import java.io.BufferedReader;
+
+/**
+ *
+ * @author lénaëlle
+ */
+public class Charger {
+     /**
+     * Nom du fichier où la sauvegarde se trouve
+     */
+    private String nomFichier;
+    
+    /**
+     * Le BufferedReader pour lire le fichier de sauvegarde
+     */
+    private BufferedReader lecteur;
+    
+    
+}
